7f828fbabfab4d513cc3ef99fe79df4144feb580
MohammadFahmawi98 <mohammaddia94@gmail.com>
2026-05-30 17:57:35 +0400

    Fix: The connection string is being loaded directly fro...

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,4 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+if (!/^(postgres:\/\/)/.test(process.env.DATABASE_URL)) { 
+    throw new Error("Invalid DATABASE_URL"); 
+}
+
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
